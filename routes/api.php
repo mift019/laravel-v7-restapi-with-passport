cd814ea2b844fddba4286438bd57eb677ed3e27e
Miftakhul Aziz <miftakhul.14116078@student.itera.ac.id>
2020-05-05 12:08:20 +0700

    Update api

--- routes/api.php
+++ routes/api.php
@@ -17,3 +17,9 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::post('login', 'Api\AuthController@login');
+Route::post('register', 'Api\AuthController@register');
+Route::middleware('auth:api')->group(function(){
+  Route::post('details', 'Api\AuthController@get_user_details_info');
+});
